81b81b71efb3db2ba8cfb4c2484e80714887793c
Idoko Emmanuel <idokoemma.ogbole@gmail.com>
2024-02-11 01:06:00 +0100

    table

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
+//import { arryMovies, getMovies } from "../page/FakeMoviesList";
 
+/*
 const arrMovies=[
   {
     id:1,
@@ -19,45 +21,88 @@ const arrMovies=[
 
   },
 ]
+*/
 
-class  Movies extends Component  {
- state= {
-  
- }
- render() {
-     return (
-        
+const arryMovies = [
+  {
+    _id: 1,
+    title: "terminator",
+    genre: { _id: 1, name: "Action" },
+    numberInStock: 6,
+    dailyRentalRate: 4.6,
+    publishDate: "2024-01-25",
+  },
+
+  {
+    _id: 2,
+    title: "emmy",
+    genre: { _id: 2, name: "Action" },
+    numberInStock: 6,
+    dailyRentalRate: 4.6,
+    publishDate: "2024-01-25",
+  },
+
+  {
+    _id: 3,
+    title: "terminator",
+    genre: { _id: 3, name: "Action" },
+    numberInStock: 5,
+    dailyRentalRate: 4.4,
+    publishDate: "2024-01-25",
+  },
+];
+
+class Movies extends Component {
+  state = {
+    arryMovies:arryMovies
+  };
+  handleDelete = (movie: any) => {
+    //console.log(movie)
+
+    const arryMovies = this.state.arryMovies.filter(
+      (m: any) => m._id!== movie._id
+    );
+    this.setState({ arryMovies });
+    
+  };
+  render() {
+    return (
       <table className="table">
-  <thead>
-    <tr>
-      
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    
-      {arrMovies.map(movie => (
-        <tr key={movie.id}>
-          
-
-          <td>{movie.first}</td>
-          <td>{movie.last}</td>
-          <td>{movie.blog}</td>
-          <td>
-            <button className="btn btn-danger">button</button>
-          </td>
-
-        </tr>
-      ))}
-    
-      
-  </tbody>
-</table>
-     )
- }
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Rate</th>
+            <th scope="col">Publiishdate</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {arryMovies.map((movie: any) => (
+            <tr key={movie._id}>
+              <td>{movie._id}</td>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>{movie.publishDate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger"
+                >
+                  {" "}
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
 
-export default Movies
+export default Movies;
---
+++ src/page/FakeMoviesList.tsx
@@ -0,0 +1,43 @@
+
+
+export const arryMovies=[
+{
+    _id: 1,
+    title:"terminator",
+    genre: {_id:1, name:"Action"},
+    numberInStock:6,
+    dailyRentalRate:4.6,
+    publishDate:"2024-01-25",
+},
+
+{
+    _id: 2,
+    title:"terminator",
+    genre: {_id:2, name:"Action"},
+    numberInStock:6,
+    dailyRentalRate:4.6,
+    publishDate:"2024-01-25",
+},
+
+{
+    _id: 3,
+    title:"terminator",
+    genre: {_id:3, name:"Action"},
+    numberInStock:5,
+    dailyRentalRate:4.4,
+    publishDate:"2024-01-25",
+}
+]
+export const getMovies=()=>{
+  return arryMovies
+}
+
+ export const FakeMoviesList = () => {
+  return (
+    <></>
+  
+    
+  )
+}
+
+export default FakeMoviesList
